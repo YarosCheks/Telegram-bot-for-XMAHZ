f78847bf4962fa930d3adb0169871c69d93a8213
n.yarek <yaroslav.chexter@gmail.com>
2024-07-17 18:46:52 +0300

    Initial commit

---
+++ src/main/java/com/example/TgBimBot/finalVariables/Links.java
@@ -0,0 +1,31 @@
+package com.example.TgBimBot.finalVariables;
+
+public class Links {
+
+    public static final String SMICH_CHANNEL_URL = "https://t.me/xmahz";
+    public static final String BIMARIUM_CHANNEL_URL = "https://t.me/xmahz";
+    public static final String T_BANK_BLACK_URL = "https://t.me/xmahz";
+    public static final String T_BANK_PLATINUM_URL = "https://t.me/xmahz";
+    public static final String T_BANK_MOBILE_URL = "https://t.me/xmahz";
+    public static final String T_BANK_INVESTMENT_URL = "https://t.me/xmahz";
+    public static final String T_BANK_BUSINESS_ACCOUNT_URL = "https://t.me/xmahz";
+    public static final String T_BANK_BUSINESS_REGISTRATION_URL = "https://t.me/xmahz";
+    public static final String T_BANK_AIR_URL = "https://t.me/xmahz";
+    public static final String T_BANK_MORTGAGE_INSURANCE_URL = "https://t.me/xmahz";
+    public static final String T_BANK_CREDIT_CASH_URL = "https://t.me/xmahz";
+    public static final String T_BANK_CREDIT_AUTO_URL = "https://t.me/xmahz";
+    public static final String T_BANK_CREDIT_BY_ESTATE_URL = "https://t.me/xmahz";
+    public static final String T_BANK_CREDIT_BY_AUTO_URL = "https://t.me/xmahz";
+    public static final String T_BANK_CREDIT_REFINANCING_URL = "https://t.me/xmahz";
+    public static final String T_BANK_CASCO_URL = "https://t.me/xmahz";
+    public static final String T_BANK_OSAGO_URL = "https://t.me/xmahz";
+    public static final String ALFA_BANK_DEBIT_URL = "https://t.me/xmahz";
+    public static final String ALFA_BANK_CREDIT_URL = "https://t.me/xmahz";
+    public static final String ALFA_BANK_BUSINESS_ACCOUNT_URL = "https://t.me/xmahz";
+    public static final String ALFA_BANK_BUSINESS_REGISTRATION_URL = "https://t.me/xmahz";
+    public static final String ALFA_BANK_ONLY_URL = "https://t.me/xmahz";
+    public static final String ALFA_BANK_TRAVEL_URL = "https://t.me/xmahz";
+    public static final String ALFA_BANK_STICKER_URL = "https://t.me/xmahz";
+    public static final String WORKS_T_BANK_URL = "https://t.me/xmahz";
+    public static final String WORKS_YANDEX_SMENA_URL = "https://t.me/xmahz";
+}
---
+++ src/main/java/com/example/TgBimBot/finalVariables/Neutral.java
@@ -0,0 +1,7 @@
+package com.example.TgBimBot.finalVariables;
+
+public class Neutral {
+
+    // тут нужен id человека, которому будут приходить /support запросы
+    public static final long adminChatID = 111111111L;
+}
